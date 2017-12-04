8a5a97716cc00cd1e181e60d042e9a48082004e2
ritmatter <mritter123@gmail.com>
2017-12-03 23:21:36 -0500

    Initial Commit

---
+++ Climbing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Climbing
+//
+//  Created by Matthew Ritter on 12/3/17.
+//  Copyright © 2017 Matthew Ritter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
